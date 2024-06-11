29d67b1561d26b8506e2c48ee3e585e52842a31d
ThePol9lK <93815581+ThePol9lK@users.noreply.github.com>
2024-06-11 21:07:10 +0300

    Update settings.py

--- smart/settings.py
+++ smart/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'smart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Smarter.mysql.pythonanywhere-services.com$default',
-        'USER': 'Smarter',
-        'PASSWORD': '$$izD7F_@w*284%',
-        'HOST': 'Smarter.mysql.pythonanywhere-services.com',
+        'NAME': 'ymnii$default',
+        'USER': 'ymnii',
+        'PASSWORD': 'Y@bu59.?u%=Uhri',
+        'HOST': 'ymnii.mysql.pythonanywhere-services.com',
         'OPTIONS': {
             'init_command': "SET NAMES 'utf8mb4';SET sql_mode = 'STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
@@ -88,7 +88,6 @@ DATABASES = {
 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
